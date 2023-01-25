b43b9629933940ba12be089b17b26fd40ebce4f9
wm07070 <wm07070@naver.com>
2023-01-25 19:40:21 +0900

    1_25-2

--- frontend/src/customer/food.js
+++ frontend/src/customer/food.js
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
 import "./food.css";
-function Food({ id, name, image, price, description }) {
+import { Link } from "react-router-dom";
+function Food({ product }) {
   const [value, setValue] = useState(0);
+  const { id, name, price, image } = product;
 
   return (
     <div className="food">
-      <div className="food_info">
-        <p>{name}</p>
-        <p className="food_price">
-          <small>가격</small>
-          <strong>{price}</strong>
-          <small>원</small>
-        </p>
-      </div>
-      <div className="button_container">
-        <button onClick={() => setValue(value - 1)}>-</button>
-        <p>수량 : {value}</p>
-        <button onClick={() => setValue(value + 1)}>+</button>
-      </div>
+      <Link to={`/detail/${id}`}>
+        <img className="food_img" src={image} alt="" />
+      </Link>
 
-      <div className="food_description">
-        <small>{description}</small>
+      <div text="text">
+        <div className="food_info">
+          <p className="food_name">{name}</p>
+          <p className="food_price">
+            <small>가격</small>
+            <strong>{price}</strong>
+            <small>원</small>
+          </p>
+        </div>
       </div>
     </div>
   );
--- frontend/src/customer/menu.js
+++ frontend/src/customer/menu.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Food from "./food";
 import "./menu.css";
 
-function Menu() {
+function Menu({ products }) {
   const navigate = useNavigate();
   const clickFood = () => {
     navigate("/menu/food");
@@ -17,13 +17,15 @@ function Menu() {
     <div>
       <Header />
       <div className="menu">
-        <button className="menu_button">메뉴</button>
-          <Food id="1" image="img/woodong.jpeg" name="치킨" price={20000}  />
-          <Food id="2" image="img/woodong.jpeg" name="마른오징어" price={16000}/>
-          <Food id="3" image="img/woodong.jpeg" name="라면" price={5000} />
-          <Food id="4" image="img/woodong.jpeg" name="황도" price={20000} />
-
+        {/*<button className="menu_button">메뉴</button>*/}
+        {/*<Food id="1" image="img/woodong.jpeg" name="치킨" price={20000} />*/}
+        {/*<Food id="2" image="img/woodong.jpeg" name="마른오징어" price={16000} />*/}
+        {/*<Food id="3" image="img/woodong.jpeg" name="라면" price={5000} />*/}
+        {products.map((product) => (
+          <Food product={product} key={product.id} />
+        ))}
       </div>
+      <det ail />
     </div>
   );
 }
