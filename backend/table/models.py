552e0d43a16eaaa172e7cb8a7998948be5b19734
sdahye <102799371+sdahye@users.noreply.github.com>
2023-01-29 15:53:43 +0900

    Update 'table'

--- backend/table/models.py
+++ backend/table/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 
-# Create your models here.
 class Table(models.Model):
     id = models.BigAutoField(help_text="Table ID", primary_key=True)
     table_state = models.BooleanField(default=True)
 
-    def __str__(self):
+    def __int__(self):
         return self.id
