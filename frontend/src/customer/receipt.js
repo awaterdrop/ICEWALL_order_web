61aed46fb207dad78066d644fd961bc15c1cb481
wm07070 <wm07070@naver.com>
2023-02-12 18:00:06 +0900

    2-12-last

--- frontend/src/customer/receipt.js
+++ frontend/src/customer/receipt.js
@@ -1,7 +1,6 @@
 import Header from "../components/header/Header";
 
 import { useSelector } from "react-redux";
-import { Table } from "react-bootstrap";
 
 import React from "react";
 import PriceForReceipt from "./PriceForReceipt";
@@ -10,27 +9,25 @@ function Receipt() {
   let state = useSelector((state) => {
     return state;
   });
+
+  let totalPrice = 0;
+  for (let i = 0; i < state.cart.length; i++) {
+    totalPrice += state.cart[i].receiptAmount * state.cart[i].price;
+  }
+
   return (
     <div>
       <Header />
       <div>
-        <Table>
-          <thead>
-            <tr>
-              <th>상품명</th>
-              <th>상품가격</th>
-            </tr>
-          </thead>
-        </Table>
+        <div>상품명</div>
+        <div>상품가격</div>
+
         <div className="cart">
-          {/*  {products.map((product) => (*/}
-          {/*    <Price product={product} key={product.id} />*/}
-          {/*  ))}*/}
           {state.cart.map((a, i) => (
             <PriceForReceipt id={i} key={i} />
           ))}
         </div>
-        <h3>총가격:</h3>
+        <h3>총가격:{totalPrice}</h3>
       </div>
     </div>
   );
